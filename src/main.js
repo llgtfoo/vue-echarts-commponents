83010d1db005150dc715a1c780000efd663630b6
lilong <lilong@hztianque.com>
2020-08-24 09:19:52 +0800

    测试组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /*
  * @Author: llgtfoo@163.com
  * @Date: 2020-07-07 22:23:47
- * @LastEditTime: 2020-07-07 22:36:53
- * @LastEditors: user
+ * @LastEditTime: 2020-08-24 09:15:13
+ * @LastEditors: Please set LastEditors
  * @Description: 
  * @FilePath: \vue-echarts-commponents\src\main.js
  */
@@ -16,6 +16,9 @@ import liquidfill from 'echarts-liquidfill'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 
+import llgtfooComponentsBox from 'llgtfoo-components-box'
+import 'llgtfoo-components-box/dist/llgtfoo-components-box.css'
+Vue.use(llgtfooComponentsBox)
 
 Vue.config.productionTip = false;
 Vue.use(ViewUI)
--- src/views/page/page/index.vue
+++ src/views/page/page/index.vue
@@ -1,23 +1,94 @@
 <!--
  * @Author: llgtfoo@163.com
  * @Date: 2020-07-07 23:11:53
- * @LastEditTime: 2020-07-09 00:14:47
- * @LastEditors: user
+ * @LastEditTime: 2020-08-24 09:19:35
+ * @LastEditors: Please set LastEditors
  * @Description: 
  * @FilePath: \vue-echarts-commponents\src\views\page\page\index.vue
  -->
 <template>
   <div class="pie-container">
      待丰富的页面！！！
+      <tab-page :data='dataLists' :option='option1'>
+        <ul class="list-box">
+          <li v-for="(item,index) in dataLists" :key="index">{{item.name}}</li>
+        </ul>
+      </tab-page>
   </div>
 </template>
 
 <script>
 export default {
-
+   data(){
+     return{
+        option1:{
+        number:5,//默认五个
+        interval:10,//必传值，与css中写的元素间隔一致
+        single:false,
+        autoScroll:false,
+        autoScrollTime:2000,
+      },
+      dataLists:[
+         {
+           name:'内容1'
+         },
+         {
+           name:'内容2'
+         },
+         {
+           name:'内容3'
+         },
+         {
+           name:'内容4'
+         },
+         {
+           name:'内容5'
+         },
+         {
+           name:'内容6'
+         },
+        //  {
+        //    name:'内容11'
+        //  },
+        //  {
+        //    name:'内容21'
+        //  },
+        //  {
+        //    name:'内容31'
+        //  },
+        //  {
+        //    name:'内容41'
+        //  },
+        //  {
+        //    name:'内容51'
+        //  },
+        //  {
+        //    name:'内容61'
+        //  },
+         
+      ],
+     }
+   }
 }
 </script>
 
 <style lang='scss' scoped>
-
+.list-box {
+    display: flex;
+    flex-wrap: nowrap;
+    height: 50px;
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    background: cyan;
+    width: 85px;
+    margin-left: 10px;
+    font-size: 16px;
+    &:first-of-type {
+      margin-left: 0;
+    }
+  }
+}
 </style>
